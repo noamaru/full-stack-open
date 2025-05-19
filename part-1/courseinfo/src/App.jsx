2f7784aaac86bb7343a678b9409371cc25319455
noa <210450274+noamaru@users.noreply.github.com>
2025-05-19 09:21:11 -0500

    Turn course into object

--- part-1/courseinfo/src/App.jsx
+++ part-1/courseinfo/src/App.jsx
@@ -1,6 +1,6 @@
 
 const Header = (props) => (
-  <h1>{props.course}</h1>
+  <h1>{props.name}</h1>
 )
 
 const Content = (props) => (
@@ -23,25 +23,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    }, {
-      name: 'Using props to pass data',
-      exercises: 7
-    }, {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts={parts}></Content>
-      <Total parts={parts}></Total>
+      <Header name={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
     </div>
   )
 }
